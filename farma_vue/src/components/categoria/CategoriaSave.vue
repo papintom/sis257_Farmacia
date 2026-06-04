2c238f57271392986845b275e7f5849a53d0c867
Paola Pinto <71159567.pp@gmail.com>
2026-06-04 14:22:03 -0400

    categoria arreglada

--- farma_vue/src/components/categoria/CategoriaSave.vue
+++ farma_vue/src/components/categoria/CategoriaSave.vue
@@ -55,39 +55,19 @@ async function handleSave() {
 
 <template>
   <div class="card flex justify-center">
-    <Dialog
-      v-model:visible="dialogVisible"
-      :header="props.modoEdicion ? 'Editar' : 'Crear'"
-      style="width: 25rem"
-    >
+    <Dialog v-model:visible="dialogVisible" :header="props.modoEdicion ? 'Editar' : 'Crear'" style="width: 25rem">
       <div class="flex items-center gap-4 mb-4">
         <label for="nombre" class="font-semibold w-3">Nombre</label>
-        <InputText
-          id="nombre"
-          v-model="categoria.nombre"
-          class="flex-auto"
-          autocomplete="off"
-          autofocus
-        />
+        <InputText id="nombre" v-model="categoria.nombre" class="flex-auto" autocomplete="off" autofocus />
       </div>
       <div class="flex items-center gap-4 mb-4">
         <label for="descripcion" class="font-semibold w-3">Descripción</label>
-        <Textarea
-          id="descripcion"
-          v-model="categoria.descripcion"
-          class="flex-auto"
-          autocomplete="off"
-        />
+        <Textarea id="descripcion" v-model="categoria.descripcion" class="flex-auto" autocomplete="off" />
       </div>
 
       <div class="flex justify-end gap-2">
-        <Button
-          type="button"
-          label="Cancelar"
-          icon="pi pi-times"
-          severity="secondary"
-          @click="dialogVisible = false"
-        ></Button>
+        <Button type="button" label="Cancelar" icon="pi pi-times" severity="secondary"
+          @click="dialogVisible = false"></Button>
         <Button type="button" label="Guardar" icon="pi pi-save" @click="handleSave"></Button>
       </div>
     </Dialog>
